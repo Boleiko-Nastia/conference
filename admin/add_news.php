53f5c9cb34861ed35281533186d06d780912cad1
Gleb <noskovgleb@mail.ru>
2016-02-01 01:20:05 +0200

    Add news functionality was made

--- admin/add_news.php
+++ admin/add_news.php
@@ -1,11 +1,42 @@
 <?php
 require_once('common.php');
 require_once('models/add_news.model.php');
-$title = $_POST['title'];
-$name = $_POST['name'];
+$data = array();
+$data['addedby'] = $_POST['name'];
 $image = $_FILES['articleimage'];
-$article = $_POST['article'];
-$langradio = $_POST['languages'];
+
 $sendnewsletter = $_POST['sendnewsletter'];
 
+if($_POST['title']){
+    if($image){
+        $info = new SplFileInfo($image['name']);
+        $image_id = add_image(array('name' => $image['name'],'type' => $info->getExtension()));
+
+        if($image_id) {
+            $uploads_dir = '../img/news-img/';
+            $uploadfile = $uploads_dir . basename($image_id).'.'.$info->getExtension();
+            move_uploaded_file($image['tmp_name'], $uploadfile);
+        }
+    }
+    $data['date_create'] = date('Y-m-d',time());
+    $data['image_id'] = $image_id;
+    $article_id = add_article($data);
+    unset($data);
+
+    $data['article_id'] = $article_id;
+    $data['title'] = $_POST['title'];
+    $data['full_text'] = $_POST['article'];
+    $lang_id = '';
+    $lang = get_lang();
+    $langradio = $_POST['languages'];
+    foreach($lang as $lk => $lv){
+        if($lv['lang'] == $langradio){
+            $lang_id = $lv['id'];
+        }
+    }
+    if($lang_id){
+        $data['lang_id'] = $lang_id;
+    }
+    add_news($data);
+}
 require_once('templates/add_news.template.php');
--- admin/models/add_news.model.php
+++ admin/models/add_news.model.php
@@ -1,1 +1,23 @@
 <?php
+function add_image($data){
+    global $db;
+    $id = $db->insert('conference_images',$data);
+    return $id;
+}
+
+function add_article($data){
+    global $db;
+    $id = $db->insert('conference_article',$data);
+    return $id;
+}
+
+function add_news($data){
+    global $db;
+    $id = $db->insert('conference_article_translation',$data);
+    return $id;
+}
+
+function get_lang(){
+    global $db;
+    return $db->get('conference_lang');
+}
