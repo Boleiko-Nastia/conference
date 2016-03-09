78a8dd7d2debc01cc334f584af5ed8bf11ce761e
Gleb <noskovgleb@mail.ru>
2016-03-09 09:21:26 +0200

    fix conflict form of  participation

--- admin/bin/add_user.php
+++ admin/bin/add_user.php
@@ -13,7 +13,8 @@ $participation_3 = $_POST['participation_3'];
 $email = $_POST['email'];
 $tel = $_POST['tel'];
 $address = $_POST['address'];
-$data = array('name'=>$name,'middlename'=>$lastname,'job'=>$study,'degree'=>$degree,'surname'=>$surename,'internally'=>$event,'email'=>$email,'address'=>$address,'phone'=>$phone,'participation'=>$participation_1.','.$participation_2.','.$participation_3);
+$position = $_POST['position'];
+$data = array('name'=>$name,'middlename'=>$lastname,'job'=>$study,'degree'=>$degree,'surname'=>$surename,'internally'=>$event,'email'=>$email,'address'=>$address,'phone'=>$phone,'participation'=>$participation_1.','.$participation_2.','.$participation_3,'position'=>$position);
 $id = add_user($data);
 if($id){
     echo 'true';
