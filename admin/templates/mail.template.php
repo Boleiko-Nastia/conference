2b59c7a15dfe9ae0c8413f4ae26964c504ec7c63
Gleb <noskovgleb@mail.ru>
2016-03-14 01:29:37 +0200

    Added mail template

---
+++ admin/templates/mail.template.php
@@ -0,0 +1,68 @@
+<html lang="en">
+  <head>
+    <meta content="text/html; charset=utf-8" http-equiv="Content-Type">
+    <title>
+      Classical
+    </title>
+	<style type="text/css">
+	a:hover { text-decoration: none !important; }
+	.header h1 {color: #fff !important; font: normal 33px Helvetica,Arial,sans-serif; margin: 0; padding: 0; line-height: 33px;}
+	.header p {color: #333; font: normal 11px Helvetica,Arial,sans-serif; margin: 0; padding: 0; line-height: 11px; letter-spacing: 2px}
+	.content h2 {color:#333 !important; font-weight: normal; margin: 0; padding: 0; font-style: italic; line-height: 30px; font-size: 30px;font-family: Helvetica,Arial,sans-serif; }
+	.content p {color:#333; font-weight: normal; margin: 0; padding: 0; line-height: 20px; font-size: 12px;font-family: Helvetica,Arial,sans-serif;}
+	.content a {color: #CE2039; text-decoration: none;}
+	.footer p {padding: 0; font-size: 11px; color:#fff; margin: 0; font-family: Helvetica,Arial,sans-serif;}
+	.footer a {color: #CE2039; text-decoration: none;}
+	</style>
+  </head>
+  <body style="margin: 0; padding: 0; background: #ddd;">
+  	<table cellpadding="0" cellspacing="0" border="0" align="center" width="100%">
+		  <tr>
+		  	<td align="center" style="margin: 0; padding: 0; background:#ddd;padding: 35px 0">
+			    <table cellpadding="0" cellspacing="0" border="0" align="center" width="650" style="font-family: Helvetica,Arial,sans-serif" class="header">
+			      <tr>
+			        <td bgcolor="#172240" height="80" align="left">
+						<a href="index.php" style="text-decoration: none;"><img src="%domain%/img/logo.png" style="margin: 0; padding: 20px;"></a>
+			        </td>
+			      </tr>
+				  <tr>
+					  <td style="font-size: 1px; height: 5px; line-height: 1px;" height="5">&nbsp;</td>
+				  </tr>	
+				</table><!-- header-->
+				<table cellpadding="0" cellspacing="0" border="0" align="center" width="650" style="font-family: Helvetica,Arial,sans-serif; background: #fff;" bgcolor="#ffffff">
+			      <tr>
+			        <td width="14" style="font-size: 0px;" bgcolor="#ffffff">&nbsp;</td>
+					<td width="620" valign="top" align="left" bgcolor="#ffffff"style="font-family: Helvetica,Arial,sans-serif; background: #fff;">
+						<table cellpadding="0" cellspacing="0" border="0"  style="color: #333; font: normal 11px Helvetica,Arial,sans-serif; margin: 0; padding: 0;" width="620" class="content">
+						<tr>
+							<td style="padding: 15px 0 15px; border-bottom: 2px solid #888;font-family: Helvetica,Arial,sans-serif; "  valign="top" align="center">
+								<h3 style="color:#444; font-weight: normal; margin: 0; padding: 0; line-height: 28px; font-size: 24px;">%title%</h3>
+							</td>
+						</tr>
+						<tr>
+							<td style="padding: 15px 0 15px;"  valign="top">
+								<p style="color:#333; font-weight: normal; margin: 0; padding: 0; line-height: 20px; font-size: 12px;font-family: Helvetica,Arial,sans-serif; ">
+									%maintext%
+								</p>
+								<br><br>
+								<a href="index.php" style="text-decoration: none;"><img src="%domain%/img/button_ukr.png" style="margin: 0; padding: 20px 0px 20px 410px;"></a>
+							</td>
+						</tr>		
+						</table>	
+					</td>
+					<td width="16" bgcolor="#ffffff" style="font-size: 0px;font-family: Helvetica,Arial,sans-serif; background: #fff;">&nbsp;</td>
+			      </tr>
+				</table><!-- body -->
+				<table cellpadding="0" cellspacing="0" border="0" align="center" width="650" style="font-family: Helvetica,Arial,sans-serif; line-height: 10px;" bgcolor="#698291" class="footer">
+			      <tr>
+			        <td bgcolor="#172240"  align="center" style="padding: 15px 0 10px; font-size: 11px; color:#fff; margin: 0; line-height: 1.2;font-family: Georgia, serif;" valign="top">
+						<p style="padding: 5px 15px; font-size: 11px; color:#fff; margin: 0; font-family: Helvetica,Arial,sans-serif;">Ви отримуєте цю розсилку, тому що є діючим або можливим учасником конференції PMW-2016 у Харківському національному університетi радіоелектроніки.</p>
+						<p style="padding: 5px 15px; font-size: 11px; color:#fff; margin: 0 0 8px 0; font-family: Helvetica,Arial,sans-serif;">Виникли труднощі з відображенням змісту листа? <webversion style="color: #CE2039; text-decoration: none;">Виконайте перегляд в своєму браузері.</webversion>.<br> Більше не цікавлять новини конференції? <unsubscribe style="color: #CE2039; text-decoration: none;">Відписатися від розсилки</unsubscribe> зараз.</p>
+					</td>
+			      </tr> 
+				</table><!-- footer-->
+		  	</td>
+		</tr>
+    </table>
+  </body>
+</html>
