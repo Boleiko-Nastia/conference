3ec965f61836342ec097caede411617df9916acf
Boleiko-Nastia <nastia_boleiko@ukr.net>
2016-03-19 17:57:07 +0200

    Updating menu

--- admin/templates/site_commission.template.php
+++ admin/templates/site_commission.template.php
@@ -43,17 +43,17 @@
         <!-- Collect the nav links, forms, and other content for toggling -->
         <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
             <ul class="nav navbar-nav navbar-right">
-                                <li>
+                <li>
                     <a href="index.php"><?=$lang['MENU_HOME']?></a>
+                </li>
+                <li>
+                    <a href="news.php"><?=$lang['NEWS']?></a>
                 </li>
                 <li>
                     <a href="sections.php"><?=$lang['CONFERENCE_AREA']?></a>
                 </li>
                 <li>
                     <a href="calendar.php"><?=$lang['CALENDAR']?></a>
-                </li>
-                <li>
-                    <a href="news.php"><?=$lang['NEWS']?></a>
                 </li>
                 <li class="active">
                     <a href="commission.php"><?=$lang['ORGANISERS']?></a>
